7094deef76167a6baa18349eb20330d91314f322
pedia <ken.shao@gmail.com>
2021-02-20 13:33:57 +0800

    upload to pypi

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import setup
 
+long_description = open('README.md').read()
+
 setup(
     name='repocache',
-    version='0.2',
-    description='Universal caching and proxying server for pypi/maven',
+    version='0.2.2',
+    description='Universal caching and proxying server for pypi and maven',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/pedia/repocache',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
     author='repocache author',
     packages=['repocache'],
     package_data={
@@ -13,6 +22,7 @@ setup(
             'templates/*.html',
         ],
     },
+    data_files=[('.', 'default.cfg')],
     install_requires=[
         'Flask',
         'filelock',
